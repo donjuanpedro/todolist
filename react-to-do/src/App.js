5692bb3a959a21f6527444931d50f3ac2ca1c2cd
Walker Lyle <walker@walkers-air.lan>
2019-09-28 12:36:34 -0500

    css fixes and create react app

---
+++ react-to-do/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+  
+}
+
+export default App;
