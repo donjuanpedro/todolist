5f6fcba6b7c04ca619cb42c60342696229941a7a
Walker Lyle <walker@walkers-air.lan>
2019-09-28 12:43:30 -0500

    list rendering

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
 
 function App() {
   const [todos, setTodos] = useState([
@@ -8,7 +10,19 @@ function App() {
     { text: "Meet friend for lunch" },
     { text: "Build really cool todo app" }
   ]);
-  
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
